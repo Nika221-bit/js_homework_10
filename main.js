87c539fc58cf0bd81949319771a619155a94c247
NikolozLekishvili <K0K0L0z1922@gmail.com>
2026-01-26 22:36:38 +0400

    wad

---
+++ main.js
@@ -0,0 +1,51 @@
+//დავალება 8.2
+
+const p1 = document.querySelector("#p1");
+const p2 = document.querySelector("#p2");
+const p3 = document.querySelector("#p3");
+
+let text1 = p1.textContent;
+let text2 = p2.textContent;
+let text3 = p3.textContent;
+
+console.log(text1);
+console.log(text2);
+console.log(text3);
+
+//დავალება 8.3
+
+const pirveli = document.querySelector("#twoParagraphs p");
+const meore = pirveli.textContent;
+console.log(meore);
+
+//დავალება 8.4
+
+const first = document.querySelector(".p ");
+const second = first.textContent;
+console.log(second);
+
+//დავალება 8.5
+
+  function button1() {
+      console.log("პირველი ღილაკი");
+   }
+
+   function button2() {
+      console.log("მეორე ღილაკი");
+   }
+
+   function button3() {
+      console.log("მესამე ღილაკი");
+   }
+
+//დავალება 8.6
+
+   function sum() { 
+        console.log(2 + 2); 
+      } 
+      function multiplication() { 
+        console.log(3 * 3); 
+      } 
+      function divide() { 
+        console.log(4 / 4); 
+      } 
